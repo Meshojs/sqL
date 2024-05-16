3a23ab67cd2fe50bee909fe80626beb5da4dc094
Mesho Hani <mesharyhani2006@gmail.com>
2024-05-16 18:42:41 +0300

    second upload

--- class.py
+++ class.py
@@ -1,17 +1,5 @@
-class Animal:
-    def sound(self):
-        pass
+print("""
 
-class Dog(Animal):
-    def sound(self):
-        return 'Woof!'
-
-class Cat(Animal):
-    def sound(self):
-        return 'Meow!'
-
-# Polymorphism in action
-animals = [Dog(), Cat()]
-
-for animal in animals:
-    print(animal.sound())
+    try number two
+      
+    """)
